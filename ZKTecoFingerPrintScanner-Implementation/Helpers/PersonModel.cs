f8a8fff5bacec9437b0e21cf57e9104bf443a966
MagcDEV <manuel117g@gmail.com>
2020-03-08 20:27:04 -0400

    Agregar archivos de proyecto.

---
+++ ZKTecoFingerPrintScanner-Implementation/Helpers/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZKTecoFingerPrintScanner_Implementation.Helpers
+{
+    public class PersonModel
+    {
+        public int CEDULA { get; set; }
+        public string NOMBRE { get; set; }
+        public string HUELLA { get; set; }
+        public int STATUS { get; set; }
+        public string FECHA_INSC { get; set; }
+        public string TIPO_INSC { get; set; } //SI ES PARA EL GYM O PARA EL KICKBOXING
+    }
+}
---
+++ ZKTecoFingerPrintScanner-Implementation/Helpers/SqliteDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace ZKTecoFingerPrintScanner_Implementation.Helpers
+{
+    public class SqliteDataAccess
+    {
+        public static List<PersonModel> BuscaCedula(int CEDULA)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PersonModel>("Select * from CONTROLGF where CEDULA = " + CEDULA + "", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+
+
+        public static void CargaHuella(int CEDU, string NOMBRE, string HUELLA, int STATUS, string FECHA_INSC, string TIPO_INSC)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into CONTROLGF (CEDULA, NOMBRE, HUELLA, STATUS, FECHA_INSC, TIPO_INSC) values " +
+                    "(" + CEDU + ", '" + NOMBRE + "', '" + HUELLA + "', " + STATUS + ", '" + FECHA_INSC + "', '" + TIPO_INSC + "')");
+
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
